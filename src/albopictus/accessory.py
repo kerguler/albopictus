8c531cf49cfefa3748eb1f8333a81f386949e347
Kamil Erguler <kerguler@gmail.com>
2017-05-29 11:34:55 -0300

    Obtain climate variables from http://vbd.cyi.ac.cy

--- src/albopictus/accessory.py
+++ src/albopictus/accessory.py
@@ -11,3 +11,48 @@ def calcEnsemble(sim):
         'mean':numpy.mean(sim,axis=0),
         'std':numpy.std(sim,axis=0)
         }
+
+def getClimate(lon,lat,times='global'):
+    """
+    Python wrapper to obtain climate variables from http://vbd.cyi.ac.cy
+
+    Parameters
+    ----------
+
+         lon: longitude
+         lat: latitude
+         times: climate dataset (default: 'global')
+                'local'     - E-OBS dataset with 1/4 deg resolution (2007-2013)
+                'global'    - EMAC dataset with 1/2 deg resolution  (2000-2010)
+                'future'    - EMAC dataset with 1/2 deg resolution  (2045-2055)
+                'farfuture' - EMAC dataset with 1/2 deg resolution  (2080-2100)
+    """
+    import json
+    import requests
+    #
+    if not (times in ['local','global','future','farfuture']):
+        print "Error: 'times' argument should be one of ['local','global','future','farfuture']"
+        return None
+    res = {
+        'local':True,
+        'global':False,
+        'future':False,
+        'farfuture':False
+        }[times]
+    #
+    req = json.dumps({
+        "lon":lon,
+        "lat":lat,
+        "times":times,
+        "res":res,
+        "sim":0,
+        "version":'PDv1.1.1'
+        })
+    r = requests.post("http://vbd.cyi.ac.cy/python", data={'request':req})
+    if r.status_code!=200:
+        print(r.status_code, r.reason)
+        return None
+    rt = json.loads(r.text)
+    if len(rt['mean_air_temp'])==0:
+        print("Error: We do not have the climate variables you requested in our database")
+    return rt
